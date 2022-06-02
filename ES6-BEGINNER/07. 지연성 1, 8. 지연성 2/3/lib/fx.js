ba2ee1a3d2a7af7531f8ad29d46f8e8cfbf08a90
DANIEL <yuntaehui77@gmail.com>
2022-06-02 22:53:07 +0900

    2022-06-02 지연성2

--- ES6-BEGINNER/07. 지연성 1, 8. 지연성 2/3/lib/fx.js
+++ ES6-BEGINNER/07. 지연성 1, 8. 지연성 2/3/lib/fx.js
@@ -3,18 +3,27 @@ const log = console.log
 const curry = f =>
   (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._)
 
-const map = curry((f, iter) => {
-  let res = []
-  for (const a of iter) {
-    res.push(f(a))
-  }
-  return res
-})
+const L = {}
 
-const filter = curry((f, iter) => {
+L.range = function* (l) {
+  let i = -1
+  while (++i < l) {
+    yield i
+  }
+}
+
+L.entries = function* (obj) {
+  for (const k in obj) yield [k, obj[k]]
+}
+
+const take = curry((l, iter) => {
   let res = []
-  for (const a of iter) {
-    if (f(a)) res.push(a)
+  iter = iter[Symbol.iterator]()
+  let cur
+  while (!(cur = iter.next()).done) {
+    const a = cur.value
+    res.push(a)
+    if (res.length == l) return res
   }
   return res
 })
@@ -23,13 +32,34 @@ const reduce = curry((f, acc, iter) => {
   if (!iter) {
     iter = acc[Symbol.iterator]()
     acc = iter.next().value
+  } else {
+    iter = iter[Symbol.iterator]()
   }
-  for (const a of iter) {
+  let cur
+  while (!(cur = iter.next()).done) {
+    const a = cur.value
     acc = f(acc, a)
   }
   return acc
 })
 
+const find = curry((f, iter) => go(
+  iter,
+  L.filter(f),
+  take(1),
+  ([a]) => a))
+
 const go = (...args) => reduce((a, f) => f(a), args)
 
 const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
+
+var add = (a, b) => a + b
+
+const range = l => {
+  let i = -1
+  let res = []
+  while (++i < l) {
+    res.push(i)
+  }
+  return res
+}
